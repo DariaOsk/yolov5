fea547b3d1271744f8f3dddfe44646214d22ec60
DariaOsk <daria.o@rambler.ru>
2022-02-06 20:52:11 +0100

    reduced head layers

--- utils/activations.py
+++ utils/activations.py
@@ -52,10 +52,10 @@ class FReLU(nn.Module):
     def __init__(self, c1, k=3):  # ch_in, kernel
         super().__init__()
         self.conv = nn.Conv2d(c1, c1, k, 1, 1, groups=c1, bias=False)
-        #self.bn = nn.BatchNorm2d(c1)                                                # BN removed
+        self.bn = nn.BatchNorm2d(c1)                                                # BN removed for V9 and V10 
 
     def forward(self, x):
-        #return torch.max(x, self.bn(self.conv(x)))
+        return torch.max(x, self.bn(self.conv(x)))
         return torch.max(x, self.conv(x))
 
 
