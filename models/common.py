12e3704f1d7b303fa3783b72e36e8384a67bab6f
DariaOsk <daria.o@rambler.ru>
2022-02-07 09:35:30 +0100

    No BatchNorm

--- models/common.py
+++ models/common.py
@@ -40,12 +40,12 @@ class Conv(nn.Module):
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, act=True):  # ch_in, ch_out, kernel, stride, padding, groups
         super().__init__()
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups=g, bias=False)
-        self.bn = nn.BatchNorm2d(c2)                                                                  # No Batchnormalization in V8, V9 Batchnorm only in standard convolution
+        #self.bn = nn.BatchNorm2d(c2)                                                                  # No Batchnormalization in V8, V12, In V9 Batchnorm only in standard convolution
         self.act = nn.SiLU() if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
 
     def forward(self, x):
-        return self.act(self.bn(self.conv(x))) #orig
-        #return self.act(self.conv(x))
+        #return self.act(self.bn(self.conv(x))) #orig
+        return self.act(self.conv(x))
 
     def forward_fuse(self, x):
         return self.act(self.conv(x))
